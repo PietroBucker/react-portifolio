038d1dcd8fc2ba478dd0c1500862b9340b2ff2c0
Pietro Bucker <pietro_177@hotmail.com>
2023-05-04 15:07:00 -0300

    conffigurando hashrouter e criando componentes iniciais

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import { Switch, Route } from 'react-router-dom';
 import './App.css';
+import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
